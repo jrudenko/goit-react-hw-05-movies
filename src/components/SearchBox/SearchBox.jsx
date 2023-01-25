08a40d995002bd855e199b3a78733bbdc600586d
Julia Rudenko <yrudenko@i.ua>
2023-01-25 12:48:51 +0200

    MoviesPage

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,6 @@
 import s from './SearchBox.module.css';
 
+
  const SearchBox = ({ value, onChange}) => {
   return (
     <form className={s.searchForm} >
@@ -13,7 +14,16 @@ import s from './SearchBox.module.css';
         onChange={(e) => onChange(e.target.value)}
         className={s.searchInput}
       />
+        <button
+          type="submit"         
+          className={s.searchButton}
+        >
+          Search
+        </button>
     </form>
   );
 };
+
+ 
+
 export default SearchBox;
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,8 +1,10 @@
 import SearchBox from "../../components/SearchBox"
+import HeadingPage from 'pages/HeadingPage';
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom"
 import { getSearchMovies } from "../../services/API"
 import { Link } from "react-router-dom";
+
 
 const MoviesPage = () => {
     const [searchParams,setSearchParams] = useSearchParams();
@@ -22,10 +24,11 @@ const MoviesPage = () => {
   };
     
     return (
-        <>
-            <SearchBox value={query} onChange={updateQueryString} />
-    <ul>
-    {movies.map((movie) => (
+        <>          
+            <ul>
+                <HeadingPage text={'Movie Search'}></HeadingPage>
+                 <SearchBox value={query} onChange={updateQueryString} />
+                {movies.map((movie) => (
         <li key={movie.id}><Link to={`/movies/${movie.id}`}>{movie.title || movie.name}</Link></li>
         ))}
         </ul>
